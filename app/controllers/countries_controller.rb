5717ffd0e4a73499e61ed5d5d6d84a48980bd146
naechrist <729jenae.christensen@gmail.com>
2021-02-04 12:24:21 -0500

    add country feature

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,6 +1,45 @@
 class CountriesController < ApplicationController
+    before_action :set_state, only: [:show, :destroy]
+    before_action :log_in_to_edit, only: [:new, :create, :destroy]
+
+    def new
+        @country = Country.new 
+    end
+
+    def create
+        @country = Country.new(country_params)
+        if @country.save #valadations happen here
+            redirect_to country_path(@country)
+        else 
+            render :new
+        end 
+    end 
+
+    def show 
+    end 
 
     def index 
         @countries = Country.alpha 
+    end 
+
+    def destroy 
+        @country.destroy
+        redirect_to countries_path
+    end 
+
+    private
+
+    def set_params
+        params.require(:country).permit(:name)
+    end 
+
+    def set_country
+        @country = Country.find_by_id(params[:id])
+    end 
+
+    def log_in_to_edit 
+        if !logged_in?
+            redirect_to login_path
+        end
     end 
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -5,4 +5,16 @@ class Location < ApplicationRecord
     has_many :users, through: :reviews 
 
     validates :place, presence: true
+
+    def state_attributes=(attributes)
+        state = State.find_or_create_by(attributes) if !attributes['place'].empty?
+    end 
+
+    def place_and_country #model methods are instance methods 
+        "#{place} - #{state.name}"
+    end     
+
+    def self.search(search)
+        where("place LIKE ? OR description LIKE ?", "%#{search}%", "%#{search}%") 
+      end
 end
